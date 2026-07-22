434adb86c9225a2f0ccf0249924bfe68f72087a5
Arush440 <arushmittal440@gmail.com>
2026-07-22 10:39:19 +0530

    Time: 0 ms (100%), Space: 47.5 MB (32.86%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int ans=0;
+       for(int i=0;i<nums.length;i++) ans^=i^nums[i];
+       ans^=nums.length;
+       return ans; 
+    }
+}

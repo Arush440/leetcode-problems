d2bf33d1ac5756cc48484ecfb7016ca32fb14f15
Arush440 <arushmittal440@gmail.com>
2026-08-19 09:35:20 +0530

    Time: 8 ms (42.93%), Space: 28.2 MB (5.11%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rotate(self, nums, k):
+       if k == 0:
+            return
+       size = len(nums)
+       k = k if k <= size else k % size
+       nums[k:], nums[:k] = nums[:size - k], nums[size - k:]
+        
+        

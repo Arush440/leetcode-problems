2b03045172a0fb2153251ece8567c8f21c3e15e1
Arush440 <arushmittal440@gmail.com>
2026-08-22 12:22:41 +0530

    Time: 1 ms (99.95%), Space: 46.6 MB (79.78%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -12,19 +12,23 @@ class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length<1) return null;
         ListNode merged=lists[0];
-        for(int i=1;i<lists.length;i++){
-            merged=merge2(merged,lists[i]);
+        int interval=1;
+        while(interval < lists.length){
+            for(int i = 0; i + interval < lists.length; i += interval * 2){
+                lists[i]=merge2(lists[i],lists[i+interval]);
+            }
+            interval*=2;
         }
-        return merged;
+        return lists[0];
     }
-    public ListNode merge2(ListNode merged,ListNode l2){
+    public ListNode merge2(ListNode l1,ListNode l2){
         ListNode dummy=new ListNode(-1);
         ListNode res=dummy;
-        while(merged!=null&&l2!=null){
-            if(merged.val<l2.val){
-                res.next=merged;
-                res=merged;
-                merged=merged.next;
+        while(l1!=null&&l2!=null){
+            if(l1.val<l2.val){
+                res.next=l1;
+                res=l1;
+                l1=l1.next;
             }
             else{
                 res.next=l2;
@@ -32,8 +36,8 @@ class Solution {
                 l2=l2.next;
             }
         }
-        if(merged!=null){
-            res.next=merged;
+        if(l1!=null){
+            res.next=l1;
         }
         if(l2!=null){
             res.next=l2;

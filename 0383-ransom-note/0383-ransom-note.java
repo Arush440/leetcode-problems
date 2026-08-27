b5114dff55a44684834bb6954053f3a2e974f62a
Arush440 <arushmittal440@gmail.com>
2026-08-27 10:25:42 +0530

    Time: 1 ms (99.96%), Space: 46.2 MB (79.32%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int hash[]=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            hash[magazine.charAt(i)-'a']+=1;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            hash[ransomNote.charAt(i)-'a']-=1;
+        }
+        for(int i=0;i<26;i++){
+            if(hash[i]<0) return false;
+        }
+        return true;
+    }
+}

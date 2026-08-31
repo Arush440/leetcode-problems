24112f4885fe11a42bcb885aa3831f8385866dfa
Arush440 <arushmittal440@gmail.com>
2026-08-31 12:11:59 +0530

    Time: 2 ms (83.4%), Space: 47.3 MB (73.96%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length<3) return false;
+        int left=0;
+        int right=arr.length -1;
+        while(left<arr.length-1){
+            if(arr[left]<arr[left+1]) left++;
+            else break;
+        }
+        while(right>left){
+            if(arr[right]<arr[right-1]) right--;
+            else return false;
+        }
+        if(left==arr.length-1 ||right==0) return false; 
+        return true;
+    }
+}
